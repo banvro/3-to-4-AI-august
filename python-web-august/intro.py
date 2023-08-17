87ecd0cf7f389e0bb6498d8c69b69ec80e9b1e7a
Naresh Kumar <banvro07@gmail.com>
2023-08-17 16:00:21 +0530

    Python new batch 1st class

---
+++ python-web-august/intro.py
@@ -0,0 +1,139 @@
+# python is a 
+    # 1) Simple
+    # 2) Gernal Perpose
+    # 3) High Lavel 
+    # 4) Object Oriented
+    # 5) Interpreated 
+    # 6) Dynamiccly typed
+    # 7) Case Senstive and
+    # 8) Screpting language.....
+
+# 1) Simple : syntex is easy...
+
+
+# 2) Gernal perpose : 
+
+
+# languages : 
+
+# 1) Low lavel lanaguage : (machine language..)   01
+
+
+# 2) High lavel lanaguage
+
+
+
+# interpreater 
+
+
+# compiler
+
+
+
+#  Object Oriented:  OOPs 
+
+
+
+# int main(){
+#     int a;
+# }
+
+# a = 
+
+# age = 10
+
+# Age = 19
+
+
+
+
+# commments :    to increase code readability
+
+# single line comments
+
+# skdfbskdfbsdjfk sddkf skjdfskfbksdf sddfsdjfbjkdfbskdf k skdfskf
+
+# multi line comments
+
+"""
+dbkajbkadjs
+dasdaksdbjasd
+asdabskdbasjdkasdb
+askdbjkadbajkdas
+dasdasdvasjdhva
+"""
+
+# print("helllloooooo")
+
+
+# print "this is me"
+
+# print("thi is me")
+
+# print("Hello world")
+
+# abc = 20
+
+# keyword  
+
+# int 
+# float
+# class
+# object
+
+
+# int main(){
+#     ashbdjhsbdkad
+#     asdkbaskdbasd
+#     asdbkasjd
+# }
+
+# a = 20
+# if a == 10:
+    
+#     print("hlo")
+
+
+
+
+# print() ==> used to print something on console...
+
+# input() ==> to get input from user
+
+# a = 20
+# b = "my data"
+# print("this isbdjksbdkasd aksd asd aksdasjd asd   a", a, b, "akjsbdkjabsdkajdbka")
+
+# input()
+
+# print(12 + 30)
+
+# a = 100
+
+# b = 500
+
+# c = a + b
+
+# print("The sum is : ", c)
+
+# name = input("Enter your name : ")
+
+# print("User enteed : ", name)
+
+# concatenation
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("ENter second number : "))
+
+usmthis = num1 + num2
+
+print("The sum is : ", usmthis)
+
+
+
+
+
+
+
+
+
