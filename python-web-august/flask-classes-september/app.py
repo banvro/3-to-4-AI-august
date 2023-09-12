421c50dff29d75fe88302f5b9f550e9a93c5bb74
Naresh Kumar <banvro07@gmail.com>
2023-09-12 03:59:30 -0700

    working with jij=nja 2

--- python-web-august/flask-classes-september/app.py
+++ python-web-august/flask-classes-september/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/car")
 def home():
     return "welcom to my flask app..."
 
@@ -12,6 +12,29 @@ def about(id):
 @app.route("/show")
 def showpage():
     return render_template("home.html")
+
+
+@app.route("/myfrom")
+def showfome():
+    return render_template("myfrom.html")
+
+
+
+@app.route("/")
+def home1():
+    return render_template("home.html")
+
+@app.route("/about-us")
+def about1():
+    return render_template("about.html")
+
+@app.route("/contact-us")
+def contact1():
+    return render_template("contact.html")
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
