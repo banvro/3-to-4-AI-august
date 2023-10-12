123e07f58a6f386029bf440de237b7505417a529
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-10-12 03:14:58 -0700

    Update urls.py

--- python-web-august/start-django/Hello/hlo/urls.py
+++ python-web-august/start-django/Hello/hlo/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("about", views.aboutus),
     path("contact", views.contact, name = "contact"),
     path("signup", views.signup, name = "signup"),
-    path("savedata", views.savedata),
+    path("savedata", views.savedata), 
+
+    path("login-page", views.loginpage, name="login"),
+    path("logoutthis", views.logoutthis),
 ]
